b6e8d297add11c4ac61cd3ca52e003b8d4267f87
Phạm Văn Huy <pvhuy84@gmail.com>
2017-01-11 16:34:42 +0700

    Create ListNewsOfflineFragment.java

---
+++ ListNewsOfflineFragment.java
@@ -0,0 +1,29 @@
+package com.example.hcd_fresher048.appdemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListNewsOfflineFragment extends Fragment {
+
+
+    public ListNewsOfflineFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_news_offline, container, false);
+    }
+
+}
