ea4c2ba7c2f64fe12b6e36b5bcfa64b77fdd90d3
Phạm Văn Huy <pvhuy84@gmail.com>
2017-01-11 16:33:47 +0700

    Create ListNewsFragment.java

---
+++ ListNewsFragment.java
@@ -0,0 +1,57 @@
+package com.example.hcd_fresher048.appdemo;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListNewsFragment extends Fragment{
+    public static final int DISPLAY_BY_LIST_VIEW = 0;
+    public static final int DISPLAY_BY_GRID_VIEW = 1;
+
+    public ListNewsFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_list_news_list_view, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getChildFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_list_news, new ListNewsFragmentDisplayInListView())
+                .commit();
+    }
+}
